50dbac9fa12cffce7149769d5a04d17df222102a
ayush8860 <upadhyayayush8860@gmail.com>
2025-07-04 00:26:40 +0530

    temporary changes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/shared/Navbar'
+
+function App() {
+  return (
+    <div>
+    <h1 className='text-red-500'>lets built portal</h1>
+    <Navbar/>
+    </div>
+
+  )
+}
+
+export default App
